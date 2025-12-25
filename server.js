74f8e9ebf6e59ee4440315d00c8f00196cb1e119
slooma-engeneer <sloomahusin@gmail.com>
2025-12-25 02:10:32 +0200

    second

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 const path = require('path')
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/UIExpert',(req,res)=>{
     res.sendFile(path.join(__dirname,'views','main.html'))
 })
